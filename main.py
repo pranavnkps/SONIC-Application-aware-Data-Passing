bcfeb10ec05452096d081db006a1fbc901ff348c
pranavnkps <pranavnkps.181co239@nitk.edu.in>
2022-04-07 23:53:10 +0530

    main.py: Add comments

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import itertools
 warnings.simplefilter('ignore', numpy.RankWarning)
 
 def regression_models(df, plot=False):
+	# This function is used to train the different regression models used to predict the DAG parameters at each stage
 
 	global SV_Exec
 	global SV_Mem
@@ -30,15 +31,21 @@ def regression_models(df, plot=False):
 		plot_regression(df,CF_Mem,"CF_Mem")
 
 def plot_regression(df, model, name):
+	# This function is used to plot and visualize the regression models generated
+
 	myline = numpy.linspace(10, 54, 100)
 	plt.scatter(df['Input Size'], df[name])
 	plt.plot(myline, model(myline))
 	plt.show()
 
 class VM:
+	# A class which defines a virtual machine
+
 	instances = []
 	id_iter = itertools.count()
 	def __init__(self, type, memory, compute):
+		# Constructor to initialize a VM
+
 		self.__class__.instances.append(weakref.proxy(self))
 		self.type = type
 		self.id = next(self.id_iter)
@@ -47,10 +54,11 @@ class VM:
 		self.lambdas = []
 
 	def assign_lambda(self, lambda_props):
+		# Function to assign a lambda function to the VM
+
 		for i in lambda_props:
 			id = i
 			memory = lambda_props[i]
-
 
 		while memory > self.memory:
 			print(self.memory)
@@ -60,14 +68,17 @@ class VM:
 		self.lambdas.append(lambda_props)
 
 	def list_lambda(self):
+		# Display the lambda functions currently assigned to the VM
+
 		print(f'VM ID : ' + str(self.id))
 		print(self.lambdas)
 
 df = pd.read_csv('dataset.csv')
 
+# Initialize fixed parameters
 block_size = 1 # 1 MB
 frame_size = 0.1 # 0.1 MB
-VM_startup_time = 4 # 40 seconds
+VM_startup_time = 4 # 4 seconds
 VM_bandwidth = 2	# 2 MBPS
 
 num_jobs = input("Enter the number of jobs ")
@@ -79,24 +90,23 @@ for i in range(int(num_jobs)):
 		video_input_size = int(input("Enter the size of the video in MBs "))
 		fanout_degree = int(video_input_size/block_size)
 
+		# Calculate the DAG parameters for the Split Video stage
 		sv_exec = SV_Exec.__call__(video_input_size)
 		sv_mem = SV_Mem.__call__(video_input_size)
-		print(sv_mem)
 
-		AWS_large_1 = VM('AWS_large', 8000, 2) # Cold Start
+		AWS_large_1 = VM('AWS_large', 8000, 2)
 		AWS_large_1.assign_lambda({'1_1': sv_mem})
 
+		# Calculate the DAG parameters for the Extract Frame stage
 		ef_exec = EF_Exec.__call__(video_input_size)
 		ef_mem = EF_Mem.__call__(video_input_size)
 
+		# Calculate the time take for different data passing methods for the Extract Frame stage
 		T_VM_EF = VM_startup_time + ef_exec*(fanout_degree)
 		T_Direct_Scatter_EF = block_size/(VM_bandwidth/fanout_degree) + ef_exec + VM_startup_time
 		# Remote Storage - no equation
 
-		# print("EF VALUES")
-		# print(T_VM_EF)
-		# print(T_Direct_Scatter_EF)
-
+		# Choosing the appropriate data passing method and assigning the lambda functions accordingly
 		if T_VM_EF < T_Direct_Scatter_EF:
 			for j in range(fanout_degree):
 				lambda_id = '2_'+ str(j+1)
@@ -109,17 +119,16 @@ for i in range(int(num_jobs)):
 				VM_EF.assign_lambda({lambda_id: ef_mem})
 			print("Data Passing Method from Split Video to Extract Frame: Direct-Passing")
 
+		# Calculate the DAG parameters for the Classify Frame stage
 		cf_exec = CF_Exec.__call__(video_input_size)
 		cf_mem = CF_Mem.__call__(video_input_size)
 
+		# Calculate the time take for different data passing methods for the Classify Frame stage
 		T_VM_CF = VM_startup_time + cf_exec*(fanout_degree)
 		T_Direct_Scatter_CF = frame_size/(VM_bandwidth/fanout_degree) + cf_exec + VM_startup_time
 		# Remote Storage - no equation
 
-		# print("CF Values")
-		# print(T_VM_CF)
-		# print(T_Direct_Scatter_CF)
-
+		# Choosing the appropriate data passing method and assigning the lambda functions accordingly
 		if T_VM_CF < T_Direct_Scatter_CF:
 			for VM_EF, count in zip(VMs_EF, range(fanout_degree)):
 				lambda_id = '3_' + str(count+1)
@@ -135,6 +144,3 @@ for i in range(int(num_jobs)):
 		print("The lambda functions assigned to each VM are: ")	
 		for A in VM.instances:
 			A.list_lambda()
-	
-# References
-# https://medium.com/google-cloud/understanding-and-profiling-gce-cold-boot-time-32c209fe86ab
